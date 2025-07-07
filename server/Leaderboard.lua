38753340a36a63f6dd1d400299e26b757b004786
chaos952 <chaos952@gmail.com>
2025-07-06 20:05:27 -0500

    getting somewhere

--- server/Leaderboard.lua
+++ server/Leaderboard.lua
@@ -23,10 +23,11 @@ function Leaderboard.update(self, user_id, new_rating, match_details)
       self.players[user_id].ranked_games_won = (self.players[user_id].games_won or 0) + v.outcome
       self.players[user_id].ranked_games_played = (self.players[user_id].ranked_games_played or 0) + 1
       self.players[user_id].inactive_counter = math.max(0, (self.players[user_id].inactive_counter or 0) - 1)
-      self.players[user_id].rd = math.min(DEVIATION_SPREAD, (DEVIATION_SPREAD / (0.5 + math.log(self.players[user_id].ranked_games_played + 1))) * 1.1 ^ self.players[user_id].inactive_counter) or DEVIATION_SPREAD
+      self.players[user_id].rd = math.min(DEVIATION_SPREAD, (DEVIATION_SPREAD / (0.5 + math.log(self.players[user_id].ranked_games_played + 1)))) * 1.1 ^ self.players[user_id].inactive_counter or DEVIATION_SPREAD
     end
   end
   logger.debug("new_rating = " .. new_rating)
+  logger.debug("new rd = " .. self.players[user_id].rd)
   logger.debug("about to write_leaderboard_file")
   write_leaderboard_file()
   logger.debug("done with Leaderboard.update")
