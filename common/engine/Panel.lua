48397b5ecd7bceaed29c71951a7f443587e909eb
chaos952 <chaos952@gmail.com>
2025-07-13 15:38:41 -0500

    bring back stealth bridging and two-stacking

--- common/engine/Panel.lua
+++ common/engine/Panel.lua
@@ -708,7 +708,7 @@ function Panel.canSwap(self)
     if self.state == "normal"
     or self.state == "swapping"
     or self.state == "falling"
-    -- or self.state == "hovering" -- had to be removed
+    or self.state == "hovering" -- IT'S BACK BABY
     or self.state == "landing" then
       return true
     else
