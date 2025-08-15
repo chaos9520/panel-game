1e6e4f93fb353ed0598175df6937b58e54629fc9
chaos952 <chaos952@gmail.com>
2025-08-14 20:00:05 -0500

    anti-wiggling fix final

--- common/engine/Stack.lua
+++ common/engine/Stack.lua
@@ -218,6 +218,8 @@ Stack =
 
     s.n_active_panels = 0
     s.n_prev_active_panels = 0
+    s.n_active_panels2 = 0
+    s.n_prev_active_panels2 = 0
 
     s.rise_timer = consts.SPEED_TO_RISE_TIME[s.speed]
 
@@ -440,6 +442,8 @@ function Stack.rollbackCopy(source, other)
   other.chain_counter = source.chain_counter
   other.n_active_panels = source.n_active_panels
   other.n_prev_active_panels = source.n_prev_active_panels
+  other.n_active_panels2 = source.n_active_panels2
+  other.n_prev_active_panels2 = source.n_prev_active_panels2
   other.rise_timer = source.rise_timer
   other.manual_raise = source.manual_raise
   other.manual_raise_yet = source.manual_raise_yet
@@ -752,6 +756,10 @@ end
 
 function Stack.hasActivePanels(self)
   return self.n_active_panels > 0 or self.n_prev_active_panels > 0
+end
+
+function Stack.hasActivePanels2(self)
+  return self.n_active_panels2 > 0 or self.n_prev_active_panels2 > 0
 end
 
 function Stack.has_falling_garbage(self)
@@ -1306,11 +1314,13 @@ function Stack.simulate(self)
     if (self.swap_1 or self.swap_2) and not swapped_this_frame then
       local canSwap = self:canSwap(self.cur_row, self.cur_col)
       if canSwap then
+        -- the following conditions must be met to block a swap. This prevents wiggling.
         if self.panels_in_top_row
           and self.health <= 2
           and self.stop_time + self.pre_stop_time <= 0
           and self.shake_time <= 0
-          and not self:hasChainingPanels() then -- this doesn't make sense logically, but it works as intended with the above clauses.
+          and not self:hasActivePanels2()
+          and not self:hasChainingPanels() then
           -- do nothing, block the swap
         else
           self:setQueuedSwapPosition(self.cur_col, self.cur_row)
@@ -1387,6 +1397,10 @@ function Stack.simulate(self)
   prof.push("updateActivePanels")
   self:updateActivePanels()
   prof.pop("updateActivePanels")
+
+  prof.push("updateActivePanels2")
+  self:updateActivePanels2()
+  prof.pop("updateActivePanels2")
 
   if self.puzzle and self.n_active_panels == 0 and self.n_prev_active_panels == 0 then
     if self:checkGameOver() then
@@ -2110,6 +2124,11 @@ function Stack.updateActivePanels(self)
   self.n_active_panels = self:getActivePanelCount()
 end
 
+function Stack.updateActivePanels2(self)
+  self.n_prev_active_panels2 = self.n_active_panels2
+  self.n_active_panels2 = self:getActivePanelCount2()
+end
+
 function Stack.getActivePanelCount(self)
   local count = 0
 
@@ -2135,6 +2154,31 @@ function Stack.getActivePanelCount(self)
           and panel.state ~= "landing" then
             count = count + 1
           end
+        end
+      end
+    end
+  end
+
+  return count
+end
+
+function Stack.getActivePanelCount2(self)
+  local count = 0
+
+  for row = 1, self.height do
+    for col = 1, self.width do
+      local panel = self.panels[row][col]
+      if panel.isGarbage then
+        if panel.state ~= "normal" then
+          count = count + 1
+        end
+      else
+        if panel.color ~= 0
+        -- dimmed is implicitly filtered by only checking in row 1 and up
+        and panel.state ~= "normal"
+        and panel.state ~= "swapping"
+        and panel.state ~= "landing" then
+          count = count + 1
         end
       end
     end
