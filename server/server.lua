23f0691f07bd8aa2f6699ce320b5f9290ef43111
chaos952 <chaos952@gmail.com>
2025-09-25 10:30:33 -0500

    minor adjustment

--- server/server.lua
+++ server/server.lua
@@ -516,7 +516,7 @@ function Server:adjust_ratings(room, winning_player_number, gameID)
       local p1_rating = leaderboard.players[players[1].user_id].rating
       local p2_rating = leaderboard.players[players[2].user_id].rating
       local opponent = Server:player_strength(p1_rating, p2_rating)
-      if opponent == 1 and math.abs(players[player_number].level - (math.min(10, math.floor(leaderboard.players[players[player_number].user_id].rating / 200)) - 1)) <= 1 then
+      if opponent == 1 and math.abs(players[player_number].level - (math.min(10, math.floor(leaderboard.players[players[player_number].user_id].rating / 200)) - 1)) <= 1 and math.abs(players[1].level - players[2].level) <= 2 then
         leaderboard.players[players[player_number].user_id].similar_strength = (leaderboard.players[players[player_number].user_id].similar_strength or 0) + 1
       end
     end
