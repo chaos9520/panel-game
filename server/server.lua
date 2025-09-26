037c6e7e3adaf9822c5c03a5fb6a5ecea5d06832
chaos952 <chaos952@gmail.com>
2025-09-26 12:05:54 -0500

    add debug lines to check for something not calculating correctly

--- server/server.lua
+++ server/server.lua
@@ -352,7 +352,9 @@ function Server:adjust_starting_rating(level)
 end
 
 function Server:player_strength(p1, p2)
-  if math.abs(p1 - p2) <= 400 then
+  local difference = math.abs(p1 - p2)
+  logger.debug("Difference: " .. difference)
+  if difference <= 400 then
     return 1
   else
     return 0
@@ -516,6 +518,8 @@ function Server:adjust_ratings(room, winning_player_number, gameID)
       local p1_rating = leaderboard.players[players[1].user_id].rating
       local p2_rating = leaderboard.players[players[2].user_id].rating
       local opponent = Server:player_strength(p1_rating, p2_rating)
+      logger.debug("Level check: " .. (math.min(10, math.floor(leaderboard.players[players[player_number].user_id].rating / 200)) - 1))
+      logger.debug("Level difference: " .. math.abs(players[1].level - players[2].level))
       if opponent == 1 and math.abs(players[player_number].level - (math.min(10, math.floor(leaderboard.players[players[player_number].user_id].rating / 200)) - 1)) <= 1 and math.abs(players[1].level - players[2].level) <= 2 then
         leaderboard.players[players[player_number].user_id].similar_strength = (leaderboard.players[players[player_number].user_id].similar_strength or 0) + 1
       end
