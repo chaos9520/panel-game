0f8bb0e0f78db08c65b5f978deb8f8e20126b6f0
chaos952 <chaos952@gmail.com>
2025-10-08 18:19:34 -0500

    more server changes

--- server/server.lua
+++ server/server.lua
@@ -511,9 +511,7 @@ function Server:adjust_ratings(room, winning_player_number, gameID)
       local p1_rating = leaderboard.players[players[1].user_id].rating
       local p2_rating = leaderboard.players[players[2].user_id].rating
       local opponent = Server:player_strength(p1_rating, p2_rating)
-      logger.debug("Level check: " .. math.min(10, math.max(1, math.floor(leaderboard.players[players[player_number].user_id].rating / 200) - 1)))
-      logger.debug("Level difference: " .. math.abs(players[1].level - players[2].level))
-      if opponent == 1 and math.abs(players[player_number].level - (math.min(10, math.max(1, math.floor(leaderboard.players[players[player_number].user_id].rating / 200) - 1)))) <= 1 and math.abs(players[1].level - players[2].level) <= 2 then
+      if opponent == 1 and players[player_number].level > (math.min(8, math.max(1, math.floor(leaderboard.players[players[player_number].user_id].rating / 200) - 1))) and math.abs(players[1].level - players[2].level) <= 2 then
         leaderboard.players[players[player_number].user_id].similar_strength = (leaderboard.players[players[player_number].user_id].similar_strength or 0) + 1
       end
     end
