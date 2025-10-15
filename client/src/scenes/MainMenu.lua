366c0269aa8f4f7ed998801df54297b7915d76af
chaos952 <chaos952@gmail.com>
2025-10-15 05:17:19 -0500

    disable online play

--- client/src/scenes/MainMenu.lua
+++ client/src/scenes/MainMenu.lua
@@ -72,8 +72,7 @@ function MainMenu:createMainMenu()
       switchToScene(ChallengeModeMenu())
     end),
     MenuItem.createButtonMenuItem("mm_2_vs_online", {""}, nil, function()
-      switchToScene(Lobby({serverIp = "pa-server.chaos952.org"}))
-      -- pa-server.chaos952.org
+      switchToScene(Lobby({serverIp = "localhost"}))
     end),
     MenuItem.createButtonMenuItem("mm_2_vs_local", nil, nil, function()
       GAME.battleRoom = BattleRoom.createLocalFromGameMode(GameModes.getPreset("TWO_PLAYER_VS"), GameBase)
